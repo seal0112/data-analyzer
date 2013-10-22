560644723fcc2925e55a5299175a6712090e74b2
joybee210@gmail.com <joybee210@gmail.com>
2013-10-22 10:55:58 +0800

    B task for hadoop_api_document

---
+++ hadoop_api_system.c
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void api_function_system ();
+void api_function_gui ();
+void api_function_cryptography ();
+
+int main () {
+
+
+	api_function_system ();
+	api_function_gui ();
+	api_function_cryptography ();
+
+}
